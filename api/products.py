260c0cb67623791fb35f60bfd0ba80ffdef5aef6
Sathiyapramod Raghavendran <sathiyapramod22@gmail.com>
2025-12-01 09:51:59 +0530

    feat: added products modules

--- api/products.py
+++ api/products.py
@@ -6,6 +6,7 @@ from models.products import Products
 
 products = APIRouter(prefix="/products", tags=["products"])
 
+
 @products.get("/")
 def get_products(dbs: Session = Depends(connect_db)):
     data = dbs.query(Products).all()
@@ -14,16 +15,43 @@ def get_products(dbs: Session = Depends(connect_db)):
 
 
 @products.get("/{id}")
-def get_product_by_id(id: int,dbs: Session = Depends(connect_db)):
+def get_product_by_id(id: int, dbs: Session = Depends(connect_db)):
     data = dbs.query(Products).filter(Products.id == id).first()
     return {"message": f"gets all product by id - {id}"}
 
 
 @products.post("/")
-def create_product(new_item: ProductSchema):
-    return {"message": "creates a product"}
+def create_product(new_item: ProductSchema, dbs: Session = Depends(connect_db)):
+
+    current_item = Products(
+        product_name=new_item.product_name, description=new_item.description
+    )
+    dbs.add(current_item)
+    dbs.commit()
+    dbs.refresh(current_item)
+    return {"message": "product created"}
 
 
 @products.put("/{id}")
 def update_product(id: int, new_item: ProductSchema):
-    return {"message": "updates product by id"}
+    valid_entry = dbs.query(Products).filter(Products.id == id).first()
+    if not data:
+        return {"message": "no data available"}
+    else:
+        valid_entry.product_name = new_item.product_name
+        valid_entry.description = new_item.description
+        dbs.commit()
+        dbs.refresh(valid_entry)
+        return {"message": "updates product by id"}
+
+
+@products.delete("/{id}")
+def delete_product(id: int):
+    valid_entry = dbs.query(Products).filter(Products.id == id).first()
+    if not data:
+        return {"message": "no data available"}
+    else:
+        dbs.delete(valid_entry)
+        dbs.commit()
+        dbs.refresh()
+        return {"message": "Deleted successfullys"}
